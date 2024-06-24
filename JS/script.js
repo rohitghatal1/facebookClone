9acdb3e78a12f075a1e779890d4ed3453ccdba3b
rohitghatal1 <rohitghatal@gmail.com>
2024-06-24 14:34:07 +0545

    added JS to switch between home and friends

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,9 @@ let mobileMenu = document.getElementById("mobileMenus");
 let mobileHeader = document.getElementById("headerAndLogo-M");
 let homeMenu = document.getElementById("homePage");
 let feeds = document.getElementById("feeds");
+let FRequestTab = document.getElementById("freindRequestTab");
+let mainTab = document.getElementById("mainTab");
+
 function openMenusMobile(){
     feeds.style.display= "none";
     document.getElementById("groups").style.display= "none";
@@ -16,4 +19,14 @@ function openHomeMenu(){
     mobileMenu.style.display = "none";
     mobileHeader.style.display = "flex";
     feeds.style.display = "block";
+}
+
+function openMainTab(){
+    mainTab.style.display = "flex";
+    FRequestTab.style.display = "none";
+}
+
+function openFriendRequestTab(){
+    mainTab.style.display = "none";
+    FRequestTab.style.display = "block";
 }
