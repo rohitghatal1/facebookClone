75c0a629e29ee4a84fa4705f45b1c496993c3066
rohitghatal1 <rohitghatal@gmail.com>
2024-06-24 15:32:19 +0545

    added mobile responsive desing for friend request section

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,11 @@
 let mobileMenu = document.getElementById("mobileMenus");
 let mobileHeader = document.getElementById("headerAndLogo-M");
+let friendMenu = document.getElementById("fReqMobile")
 let homeMenu = document.getElementById("homePage");
 let feeds = document.getElementById("feeds");
 let FRequestTab = document.getElementById("freindRequestTab");
 let mainTab = document.getElementById("mainTab");
 
-function openMenusMobile(){
-    feeds.style.display= "none";
-    document.getElementById("groups").style.display= "none";
-    mobileHeader.style.display = "none";
-
-    mobileMenu.style.display = "block";
-    mobileMenu.style.height = "100vh";
-    mobileMenu.style.width = "100%";
-}
-
-function openHomeMenu(){
-    mobileMenu.style.display = "none";
-    mobileHeader.style.display = "flex";
-    feeds.style.display = "block";
-}
 
 function openMainTab(){
     mainTab.style.display = "flex";
@@ -29,4 +15,29 @@ function openMainTab(){
 function openFriendRequestTab(){
     mainTab.style.display = "none";
     FRequestTab.style.display = "block";
+}
+
+// for mobile devices
+function openHomeMenu(){
+    mobileMenu.style.display = "none";
+    friendMenu.style.display = "none";
+    mobileHeader.style.display = "flex";
+    // feeds.style.display = "block";
+}
+
+function openMenusMobile(){
+    feeds.style.display= "none";
+    document.getElementById("groups").style.display= "none";
+    mobileHeader.style.display = "none";
+    friendMenu.style.display = "none";
+    mobileMenu.style.display = "block";
+    mobileMenu.style.height = "100vh";
+    mobileMenu.style.width = "100%";
+}
+
+function openFriendMenu(){
+    mainTab.style.display = "none";
+    mobileHeader.style.display = "none";
+    mobileMenu.style.display = "none"
+    friendMenu.style.display = "block";
 }
